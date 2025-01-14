22a1f5471834f5dca7bc2e63af787e2424355ea1
IBS Developer <ibsdeveloper@IBSs-Mac-mini.local>
2025-01-14 16:54:38 +0800

    feat: added splash screen and launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:linguana/screens/home.dart';
 
@@ -11,13 +12,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Linguana',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
+        title: 'Linguana',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          useMaterial3: true,
+        ),
+        home: FlutterSplashScreen.fadeIn(
+          useImmersiveMode: true,
+          backgroundColor: Colors.green.shade300,
+          childWidget: SizedBox(
+            height: 200,
+            width: 200,
+            child: Image.asset("assets/images/logo.png"),
+          ),
+          nextScreen: const HomePage(),
+        ));
   }
 }
